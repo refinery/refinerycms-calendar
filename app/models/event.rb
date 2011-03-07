9c84f321de4764d7214c45e94ca33e39399b47a9
Joe Sak <joe@joesak.com>
2011-03-07 13:03:32 -0600

    Friendly ID

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,5 +5,7 @@ class Event < ActiveRecord::Base
   validates :title, :presence => true, :uniqueness => true
   validates :ticket_price, :numericality => true
   
+  has_friendly_id :title, :use_slug => true
+  
   belongs_to :image
 end
