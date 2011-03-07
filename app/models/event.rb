382026e52280aab63e86fc8e5ca30464b9f08b03
Joe Sak <joe@joesak.com>
2011-03-07 14:15:28 -0600

    Archive scope added

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,10 +1,11 @@
 class Event < ActiveRecord::Base
   default_scope order('start_at ASC')
   
-  scope :current, where(['end_at > ?', Time.now])
-  scope :upcoming, where(['start_at > ?', Time.now])
+  scope :current, where(['end_at >= ?', Time.now])
+  scope :upcoming, where(['start_at >= ?', Time.now])
   scope :featured, where(['featured IS NOT NULL and featured = ?', true])
   scope :not_featured, where(['featured IS NULL or featured = ?', false])
+  scope :archive, where(['end_at < ?', Time.now])
   
   acts_as_indexed :fields => [:title, :venue_name, :venue_address, :ticket_link, :description]
 
