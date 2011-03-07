08982aadbc4fc3144b30f27bced38ae4d850ec9d
Joe Sak <joe@joesak.com>
2011-03-07 17:16:40 -0600

    Really, we're not making events sortable that's dumb

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,6 @@ Refinery::Application.routes.draw do
   resources :events, :only => [:index, :show]
 
   scope(:path => 'refinery', :as => 'admin', :module => 'admin') do
-    resources :events, :except => :show do
-      collection do
-        post :update_positions
-      end
-    end
+    resources :events, :except => :show
   end
 end
