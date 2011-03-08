18564c3cb96aa283c9acbf77275291a285ad97f6
Joe Sak <joe@joesak.com>
2011-03-08 11:58:19 -0600

    Routes updated for #create

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Refinery::Application.routes.draw do
     get 'archive/:year(/:month)' => 'events#archive', :as => 'archive', :on => :collection
   end
   
-  post "comments" => "comments#create"
+  post "events/:id/comments" => "comments#create", :as => 'comments'
   post "comments/:id/reply" => "comments#reply", :as => 'comment_reply'
 
   scope(:path => 'refinery', :as => 'admin', :module => 'admin') do
