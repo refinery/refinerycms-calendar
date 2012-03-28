25afb9e3f294ac696ce122f5335d0cb407abbbe4
Joe Sak <joe@joesak.com>
2012-03-28 14:57:28 -0500

    Update generator and add initializer

--- lib/generators/refinery/calendar_generator.rb
+++ lib/generators/refinery/calendar_generator.rb
@@ -1,9 +1,20 @@
 module Refinery
   class CalendarGenerator < Rails::Generators::Base
 
-    def rake_db
-      rake("refinery_calendar:install:migrations")
+    def create_initializer_file
+      create_file 'config/initializers/refinery/calendar.rb',
+        <<-EOH
+REFINERY_CALENDAR = Refinery::Calendar::CoreCalendar.new
+
+Refinery::Calendar.configure do |config|
+  config.title = 'RefineryCMS Calendar'
+end
+        EOH
     end
+
+#    def rake_db
+#      rake("refinery_calendar:install:migrations")
+#    end
 
     def append_load_seed_data
       create_file 'db/seeds.rb' unless File.exists?(File.join(destination_root, 'db', 'seeds.rb'))
