d7a2acb2564c10a9e4c787a2d062dcd84563cdcb
Joe Sak <joe@joesak.com>
2012-05-30 10:26:26 -0500

    Remove old 1.0 code

--- app/controllers/admin/event_categories_controller.rb
+++
@@ -1,9 +0,0 @@
-module Admin
-  class EventCategoriesController < Admin::BaseController
-
-    crudify :event_category,
-            :title_attribute => :name,
-            :xhr_paging => true
-
-  end
-end
