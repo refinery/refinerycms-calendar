201c2c0024cc9dcd470aefe168a452256d48244c
Luis Eduardo Rodríguez Mendo <luis.mendo@the-cocktail.com>
2012-08-24 13:40:05 +0200

    Testing events with i18n.

--- spec/requests/refinery/calendar/admin/events_spec.rb
+++ spec/requests/refinery/calendar/admin/events_spec.rb
@@ -95,6 +95,160 @@ describe Refinery do
           end
         end
 
+        context "with translations" do
+
+          before(:each) do
+            Globalize.locale = :en
+            Refinery::I18n.stub(:frontend_locales).and_return([:en, :es])
+            event_page = Factory.create(:page, :link_url => "/event", :title => "Event")
+            Globalize.with_locale(:es) do
+              event_page.title = 'Evento'
+              event_page.save
+            end
+            visit refinery.calendar_admin_events_path
+          end
+
+          describe "add a event with title for default locale" do
+            before do
+              click_link "Add New Event"
+              fill_in "Title", :with => "Event"
+              fill_in "From", :with => I18n.l(1.day.from_now, :format => :long)
+              fill_in "To", :with => I18n.l(2.day.from_now, :format => :long)
+              click_button "Save"
+              @e = Refinery::Calendar::Event.find_by_title("Event")
+            end
+
+            it "succeeds" do
+              page.should have_content("'Event' was successfully added.")
+              Refinery::Calendar::Event.count.should eq(1)
+            end
+
+            it "shows locale flag for event" do
+              within "#event_#{@e.id}" do
+                page.should have_css("img[src='/assets/refinery/icons/flags/en.png']")
+              end
+            end
+
+            it "shows up in event page for default locale" do
+              visit refinery.calendar_events_path
+              page.should have_selector("#event_#{@e.id}")
+            end
+
+            it "does not show up in event page for secondary locale" do
+              visit refinery.calendar_events_path(:locale => :es)
+              page.should_not have_selector("#event_#{@e.id}")
+            end
+          end
+
+          describe "add a event with title only for secondary locale" do
+
+            let(:es_event_title) { 'Evento' }
+
+            before do
+              click_link "Add New Event"
+              within "#switch_locale_picker" do
+                click_link "Es"
+              end
+              fill_in "Title", :with => es_event_title
+              fill_in "From", :with => I18n.l(1.day.from_now, :format => :long)
+              fill_in "To", :with => I18n.l(2.day.from_now, :format => :long)
+              click_button "Save"
+              @e = Refinery::Calendar::Event.find_by_title(es_event_title)
+            end
+
+            it "succeeds" do
+              page.should have_content("'#{es_event_title}' was successfully added.")
+              Refinery::Calendar::Event.count.should eq(1)
+            end
+
+            it "shows title in secondary locale" do
+              within "#event_#{@e.id}" do
+                page.should have_content(es_event_title)
+              end
+            end
+
+            it "shows locale flag for event" do
+              within "#event_#{@e.id}" do
+                page.should have_css("img[src='/assets/refinery/icons/flags/es.png']")
+              end
+            end
+
+            it "does not show locale flag for primary locale" do
+              within "#event_#{@e.id}" do
+                page.should_not have_css("img[src='/assets/refinery/icons/flags/en.png']")
+              end
+            end
+
+            it "does not show up in event page for default locale" do
+              visit refinery.calendar_events_path
+              page.should_not have_selector("#event_#{@e.id}")
+            end
+
+            it "shows up in blog page for secondary locale" do
+              visit refinery.calendar_events_path(:locale => :es)
+              page.should have_selector("#event_#{@e.id}")
+            end
+
+          end
+
+          context "with a event in both locales" do
+
+            let!(:event) do
+              _event = Globalize.with_locale(:en) { FactoryGirl.create(:event,
+                                                      :title => "UniqueTitleOne",
+                                                      :from => 1.day.from_now,
+                                                      :to => 2.day.from_now) }
+              Globalize.with_locale(:es) do
+                _event.title = 'Título unico uno'
+                _event.save
+              end
+              _event
+            end
+
+            before(:each) do
+              visit refinery.calendar_admin_events_path
+            end
+
+            it "shows both locale flags for event" do
+              within "#event_#{event.id}" do
+                page.should have_css("img[src='/assets/refinery/icons/flags/en.png']")
+                page.should have_css("img[src='/assets/refinery/icons/flags/es.png']")
+              end
+            end
+
+            describe "edit the event in english" do
+              it "succeeds" do
+
+                within "#event_#{event.id}" do
+                  click_link("En")
+                end
+                current_path.should == refinery.edit_calendar_admin_event_path(event)
+                fill_in "Title", :with => "New Event Title"
+                click_button "Save"
+
+                page.should_not have_content(event.title)
+                page.should have_content("'New Event Title' was successfully updated.")
+              end
+            end
+
+            describe "edit the event in secondary locale" do
+              it "succeeds" do
+                within "#event_#{event.id}" do
+                  click_link("Es")
+                end
+
+                fill_in "Title", :with => "Nuevo título del evento"
+                click_button "Save"
+
+                page.should_not have_content(event.title)
+                page.should have_content("'Nuevo título del evento' was successfully updated.")
+              end
+            end
+
+          end
+
+        end
+
       end
     end
   end
