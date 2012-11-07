b878275d42993de6131c8b4b4ee8375474afd112
David J. Brenes <david.brenes@simplelogica.net>
2012-11-07 10:15:14 +0100

    Since starts_at and ends_at attribute are date and time on_day scope has to be changed

--- app/models/refinery/calendar/event.rb
+++ app/models/refinery/calendar/event.rb
@@ -21,7 +21,10 @@ module Refinery
                 :prefix => true,
                 :allow_nil => true
 
-      scope :on_day, lambda {|day| where('(refinery_calendar_events.starts_at = ?) OR (refinery_calendar_events.ends_at = ?) OR (refinery_calendar_events.starts_at < ? AND (refinery_calendar_events.ends_at > ?))', day, day, day, day) }
+      scope :on_day, lambda {|day| where('
+        ( (refinery_calendar_events.starts_at >= ?) AND (refinery_calendar_events.starts_at < ?))
+        OR ((refinery_calendar_events.ends_at >= ?) AND (refinery_calendar_events.ends_at < ?))
+        OR (refinery_calendar_events.starts_at < ? AND (refinery_calendar_events.ends_at > ?))', day, day + 1.day, day, day + 1.day, day, day) }
 
       class << self
         def upcoming
--- spec/models/refinery/calendar/event_spec.rb
+++ spec/models/refinery/calendar/event_spec.rb
@@ -5,14 +5,51 @@ module Refinery
     describe Event do
       describe "validations" do
         subject do
-          FactoryGirl.create(:event,
-          :title => "Refinery CMS")
+          FactoryGirl.create(:event, :title => "Refinery CMS")
         end
 
         it { should be_valid }
         its(:errors) { should be_empty }
         its(:title) { should == "Refinery CMS" }
+
       end
+
+      describe "upcoming events" do
+
+        before :each do
+
+          FactoryGirl.create(:event, starts_at: 2.hours.ago)
+          FactoryGirl.create(:event, starts_at: 2.hours.from_now)
+          FactoryGirl.create(:event, starts_at: 2.days.from_now)
+          FactoryGirl.create(:event, starts_at: 2.weeks.from_now)
+
+        end
+
+        subject { Event.upcoming }
+
+        its(:length) { should == 3 }
+
+      end
+
+      describe "tomorrow events" do
+
+        before :each do
+
+          FactoryGirl.create(:event, starts_at: Date.tomorrow - 2.hours)
+          FactoryGirl.create(:event, starts_at: Date.tomorrow + 2.hours)
+          FactoryGirl.create(:event, starts_at: Date.tomorrow + 6.hours)
+          FactoryGirl.create(:event, starts_at: Date.tomorrow + 12.hours)
+          FactoryGirl.create(:event, starts_at: Date.yesterday, ends_at: 2.days.from_now)
+          FactoryGirl.create(:event, starts_at: Date.tomorrow + 2.days)
+
+        end
+
+        subject { Event.on_day(Date.tomorrow) }
+
+        its(:length) { should == 4 }
+
+      end
+
     end
   end
 end
