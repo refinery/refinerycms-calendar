14d72d397abefd39947d6a2df6a43384e9f4595b
David J. Brenes <david.brenes@simplelogica.net>
2012-11-07 13:39:19 +0100

    New scopes starting_on_day and ends_on_day built with arel sintax and rewritten the on_day scope

--- app/models/refinery/calendar/event.rb
+++ app/models/refinery/calendar/event.rb
@@ -21,10 +21,16 @@ module Refinery
                 :prefix => true,
                 :allow_nil => true
 
-      scope :on_day, lambda {|day| where('
-        ( (refinery_calendar_events.starts_at >= ?) AND (refinery_calendar_events.starts_at < ?))
-        OR ((refinery_calendar_events.ends_at >= ?) AND (refinery_calendar_events.ends_at < ?))
-        OR (refinery_calendar_events.starts_at < ? AND (refinery_calendar_events.ends_at > ?))', day, day + 1.day, day, day + 1.day, day, day) }
+      scope :starting_on_day, lambda {|day| where(starts_at: day.beginning_of_day..day.tomorrow.beginning_of_day) }
+      scope :ending_on_day, lambda {|day| where(ends_at: day.beginning_of_day..day.tomorrow.beginning_of_day) }
+
+      scope :on_day, lambda {|day|
+        where(
+          arel_table[:starts_at].in(day.beginning_of_day..day.tomorrow.beginning_of_day).
+          or(arel_table[:ends_at].in(day.beginning_of_day..day.tomorrow.beginning_of_day)).
+          or( arel_table[:starts_at].lt(day.beginning_of_day).and(arel_table[:ends_at].gt(day.tomorrow.beginning_of_day)) )
+        )
+      }
 
       class << self
         def upcoming
