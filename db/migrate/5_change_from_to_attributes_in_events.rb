605d97c6e3fba3b40ad1bd485a81eb007c82850d
David J. Brenes <david.brenes@simplelogica.net>
2012-11-07 15:36:18 +0100

    Fixing issue #3: Migration now run properly on mysql

--- db/migrate/5_change_from_to_attributes_in_events.rb
+++ db/migrate/5_change_from_to_attributes_in_events.rb
@@ -4,22 +4,22 @@ class ChangeFromToAttributesInEvents < ActiveRecord::Migration
     add_column :refinery_calendar_events, :starts_at, :datetime
     add_column :refinery_calendar_events, :ends_at, :datetime
 
-    Refinery::Calendar::Event.update_all '"starts_at" = "from", "ends_at" = "to"'
+    Refinery::Calendar::Event.update_all '`starts_at` = `from`, `ends_at` = `to`'
 
-    remove_column :refinery_calendar_events, :from, :datetime
-    remove_column :refinery_calendar_events, :to, :datetime
+    remove_column :refinery_calendar_events, :from
+    remove_column :refinery_calendar_events, :to
 
   end
 
   def down
 
-    add_column :refinery_calendar_events, :starts_at, :datetime
-    add_column :refinery_calendar_events, :ends_at, :datetime
+    add_column :refinery_calendar_events, :from, :date
+    add_column :refinery_calendar_events, :to, :date
 
-    Refinery::Calendar::Event.update_all '"starts_at" = "from", "ends_at" = "to"'
+    Refinery::Calendar::Event.update_all '`from` = `starts_at`, `to` = `ends_at`'
 
-    remove_column :refinery_calendar_events, :from, :datetime
-    remove_column :refinery_calendar_events, :to, :datetime
+    remove_column :refinery_calendar_events, :starts_at
+    remove_column :refinery_calendar_events, :ends_at
 
   end
 end
