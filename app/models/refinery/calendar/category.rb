e926e571bfff0b968f11b98c7c0a08dfdfcb4fb5
Daniel Reszka <dannn.r@gmail.com>
2014-01-25 13:31:32 +0100

    Add translations support to Refinery::Calendar::Category model

--- app/models/refinery/calendar/category.rb
+++ app/models/refinery/calendar/category.rb
@@ -4,6 +4,14 @@ module Refinery
       has_and_belongs_to_many :events
       validates :name, presence: true, uniqueness: true
       attr_accessible :name, :position
+
+      translates :name
+      class Translation
+        attr_accessible :locale
+      end
+      acts_as_indexed :fields => [:name]
+
+
     end
   end
 end
