8438bc792cf6430a7afcb77e6ee580805282f84a
Daniel Reszka <dannn.r@gmail.com>
2014-01-25 13:27:05 +0100

    Add migration for calendar categories translations

---
+++ db/migrate/10_create_calendar_category_translations.rb
@@ -0,0 +1,16 @@
+class CreateCalendarCategoryTranslations < ActiveRecord::Migration
+
+  def self.up
+    Refinery::Calendar::Category.create_translation_table!({
+      name: :string,
+    }, migrate_data: true)
+
+    remove_column :refinery_calendar_categories, :name
+  end
+
+  def self.down
+    add_column :refinery_calendar_categories, :name, :string
+
+    CalendarCategorys.drop_translation_table! migrate_data: true
+  end
+end
