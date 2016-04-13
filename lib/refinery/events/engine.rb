7b9bad1354ad80615552f12ad83496fd20adf8ae
Brice Sanchez <contact@brice-sanchez.com>
2016-04-12 21:59:59 -0400

    Remove deprecated plugin.activity

--- lib/refinery/events/engine.rb
+++ lib/refinery/events/engine.rb
@@ -11,9 +11,6 @@ module Refinery
           plugin.name = "calendar"
           plugin.url = proc { Refinery::Core::Engine.routes.url_helpers.calendar_admin_events_path }
           plugin.pathname = root
-          plugin.activity = {
-            :class_name => :'refinery/calendar/event'
-          }
           plugin.menu_match = %r{refinery/calendar(/.*)?$}
         end
       end
