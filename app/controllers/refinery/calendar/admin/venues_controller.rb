2efa96a7d5cde1159ee067ff26fbe2e04ff22077
Brice Sanchez <contact@brice-sanchez.com>
2016-04-12 22:24:49 -0400

    Now use strong parameters

--- app/controllers/refinery/calendar/admin/venues_controller.rb
+++ app/controllers/refinery/calendar/admin/venues_controller.rb
@@ -7,6 +7,20 @@ module Refinery
                 title_attribute: 'name',
                 sortable: false,
                 order: 'created_at DESC'
+
+        protected
+
+        def venue_params
+          params.require(:venue).permit(permitted_venue_params)
+        end
+
+        private
+
+        def permitted_venue_params
+          [
+            :name, :address, :url, :phone, :position
+          ]
+        end
       end
     end
   end
